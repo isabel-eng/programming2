0234edc6852871da43ae9aa96e2772a6cc22a828
isabel-eng <59751947+isabel-eng@users.noreply.github.com>
2020-07-22 22:27:25 -0500

    cross product

---
+++ numpy vectorization/CrossProduct.py
@@ -0,0 +1,5 @@
+import numpy as np
+vector1 = np.array([2,6,7])
+vector2 = np.array([5,3,8])
+print ("The cross product of vector1 x vector2:", np.cross(vector1, vector2))
+print ("\nThe cross product of vector2 x vector1:", np.cross(vector2, vector1))
